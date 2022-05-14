6948b04575d4c56714d1a48f9e010bd0a0784b65
Lisa Geist <lbgeist@gmail.com>
2022-05-14 09:12:12 -0400

    adding parts of speech to script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Testing. 1-2-3 testing');
+/* inspirational message generator. Parts of the message will be combined how? 
+*/
+const adverbs = ['never', 'always', 'sometimes', 'remember'];
+const verbs = ['keep', 'try', 'wait', 'understand', 'can', 'should'];
+const subjectPronouns = ['you', 'we', 'they', 'he', 'she'];
+const objectPronouns = ['you', 'him', 'her', 'us', 'them'];
+const conjunctions = ['and', 'but', 'if'];
+const prepositions = ['at', 'by', 'for', 'from', 'in', 'of', 'on', 'to', 'with'];
+
+
+
+
+
+
+
+
+
