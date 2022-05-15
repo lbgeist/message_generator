57188e7fc8cbcb37208726914dffdc5c76ec9022
Lisa Geist <lbgeist@gmail.com>
2022-05-15 11:02:54 -0400

    final draft

--- script.js
+++ script.js
@@ -7,11 +7,27 @@ const objectPronouns = ['you', 'him', 'her', 'us', 'them'];
 const conjunctions = ['and', 'but', 'if'];
 const prepositions = ['at', 'by', 'for', 'from', 'in', 'of', 'on', 'to', 'with'];
 
+const randomize = (part) => {
+  const randomNum = Math.floor(Math.random() * part.length);
+  return part[randomNum];
+}
 
+function titleCase(string){
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
 
+const firstWord = titleCase(randomize(subjectPronouns));
+const secondWord = randomize(adverbs);
+const thirdWord = randomize(verbs);
+const fourthWord = randomize(objectPronouns);
+const fifthWord = randomize(conjunctions);
+const sixthWord = randomize(subjectPronouns);
+const seventhWord = randomize(verbs);
+const eighthWord = randomize(prepositions);
+const ninthWord = randomize(verbs);
+const tenthWord = randomize(objectPronouns);
 
-
-
+console.log(`${firstWord} ${secondWord} ${thirdWord} ${fourthWord} ${fifthWord} ${sixthWord} ${seventhWord} ${eighthWord} ${ninthWord} ${tenthWord}.`);
 
 
 
